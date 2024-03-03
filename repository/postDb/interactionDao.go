fa0f9d754ab795d0d1c61ebed4d01e681467750b
os30722 <os30722@gmail.com>
2024-03-04 00:39:48 +0530

    changes

--- repository/postDb/interactionDao.go
+++ repository/postDb/interactionDao.go
@@ -43,10 +43,62 @@ func (pd postDao) UnlikePost(ctx context.Context, postId int, userId int) error 
 	}
 
 	if commandTag.RowsAffected() == 0 {
-		return errors.New("Row not found")
+		return errors.New("row not found")
 	}
 
 	_, err = tx.Exec(ctx, "update posts set likes=likes-1 where post_id=$1", postId)
+	if err != nil {
+		return err
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (pd postDao) AddComment(ctx context.Context, postId int, userId int, content string) error {
+	var db = pd.db
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	_, err = tx.Exec(ctx, "insert into comments(post_id,user_id,content) values($1,$2$3)", postId, userId, content)
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(ctx, "update posts set coments=comments+1 where post_id=$1", postId)
+	if err != nil {
+		return err
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (pd postDao) RemoveCommment(ctx context.Context, userId int, commentId int) error {
+	var db = pd.db
+	tx, err := db.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	commandTag, err := tx.Exec(ctx, "delete from comments where comment_id=$1 and user_id=$2 returning post_id", commentId, userId)
+	if err != nil {
+		return err
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		return errors.New("row not found")
+	}
+
+	_, err = tx.Exec(ctx, "update posts set comments=comments-1 where post_id=$1", postId)
 	if err != nil {
 		return err
 	}
